9396f9d77b54cef62e674dba4869ee59e36fafd4
Jorge Gonzalez <jorgegonzalez97d@gmail.com>
2023-09-30 01:57:13 +0200

    Añadiendo cors

--- BackendIdiomas/BackendIdiomas.Api/Program.cs
+++ BackendIdiomas/BackendIdiomas.Api/Program.cs
@@ -1,4 +1,6 @@
-using BackendIdiomas.Application.Services.Authentication;
+// using BackendIdiomas.Application.Services.Authentication;
+using BackendIdiomas.Application;
+using BackendIdiomas.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -6,7 +8,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddScoped<IAuthenticationService, AuthenticationService>();
+// builder.Services.AddScoped<IAuthenticationService, AuthenticationService>();
+builder.Services.AddApplication().AddInfrastructure();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -17,6 +20,9 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+    app.UseCors(x=>x.WithOrigins("http://localhost:4200")
+        .AllowAnyMethod()
+        .AllowAnyHeader());
 }
 
 app.UseHttpsRedirection();
--- BackendIdiomas/BackendIdiomas.Application/DependencyInjection.cs
+++ BackendIdiomas/BackendIdiomas.Application/DependencyInjection.cs
@@ -1,7 +1,11 @@
+using BackendIdiomas.Application.Services.Authentication;
+using Microsoft.Extensions.DependencyInjection;
+
 namespace BackendIdiomas.Application;
 
 public static class DependencyInjection{
     public static IServiceCollection AddApplication(this IServiceCollection services){
-        
+        services.AddScoped<IAuthenticationService, AuthenticationService>();
+        return services;
     }
 }
